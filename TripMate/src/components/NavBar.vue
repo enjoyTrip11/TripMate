49e1c037e29d85aae52bcced1e189c71449fcc5f
yeondori <yeonseo.jung0@gmail.com>
2024-05-18 16:35:53 +0900

    add: logo git 추가

--- TripMate/src/components/NavBar.vue
+++ TripMate/src/components/NavBar.vue
@@ -1,15 +1,54 @@
-<template lang="">
-    <div>
-        
-    </div>
-</template>
-
-<script>
-export default {
-    
-}
-</script>
-
-<style lang="">
-    
-</style>
+<template>
+    <nav class="navbar">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="/">TripMate</a>
+      </div>
+      <div class="navbar-menu">
+        <a class="navbar-item" href="/">Home</a>
+        <a class="navbar-item" href="/destinations">Destinations</a>
+        <a class="navbar-item" href="/plans">Plans</a>
+        <a class="navbar-item" href="/about">About</a>
+        <a class="navbar-item" href="/contact">Contact</a>
+      </div>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NavBar',
+  };
+  </script>
+  
+  <style scoped>
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0.5rem 1rem;
+    background-color: #3f51b5;
+    color: white;
+  }
+  
+  .navbar-brand .navbar-item {
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: white;
+    text-decoration: none;
+  }
+  
+  .navbar-menu {
+    display: flex;
+    gap: 1rem;
+  }
+  
+  .navbar-item {
+    color: white;
+    text-decoration: none;
+    font-size: 1rem;
+  }
+  
+  .navbar-item:hover {
+    text-decoration: underline;
+  }
+  </style>
+  
