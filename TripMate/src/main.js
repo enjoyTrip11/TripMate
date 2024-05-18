7e090e1d9a60857543c69a4836acbaddd85a8d9e
yeondori <yeonseo.jung0@gmail.com>
2024-05-19 01:05:48 +0900

    refactor: 불필요한 import 삭제

--- TripMate/src/main.js
+++ TripMate/src/main.js
@@ -3,10 +3,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-import VueFullPage from "vue-fullpage.js";
 
 const app = createApp(App)
 
 app.use(vuetify)
-app.use(VueFullPage); 
 app.mount('#app')
