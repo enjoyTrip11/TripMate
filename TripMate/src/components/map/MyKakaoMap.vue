773bd710e3942e0287b5ce1cf0e4d3d4c3fe2ef3
yeondori <yeonseo.jung0@gmail.com>
2024-05-23 00:28:50 +0900

    feat: Map 연동

---
+++ TripMate/src/components/map/MyKakaoMap.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container fluid
+  >
+    <KakaoMap :lat="coordinate.lat" :lng="coordinate.lng" :draggable="true" width="100%">
+      <KakaoMapMarker :lat="coordinate.lat" :lng="coordinate.lng"></KakaoMapMarker>
+    </KakaoMap>
+  </v-container>
+</template>
+
+<script setup>
+import { KakaoMap, KakaoMapMarker } from 'vue3-kakao-maps';
+
+const coordinate = {
+  lat: 37.566826,
+  lng: 126.9786567
+};
+
+</script>
+
+<style scoped>
+</style>
--- TripMate/src/views/map/MapMainPage.vue
+++ TripMate/src/views/map/MapMainPage.vue
@@ -1,20 +1,23 @@
 <template>
     <v-container fluid>
-       <!-- 여행지 정보 제목 -->
-       <v-row justify="center" class="mb-4">
-              <v-col cols="auto">
-                  <h1 class="display-1">여행지 정보</h1>
-                  <hr>
-              </v-col>
-          </v-row>
+        <!-- 여행지 정보 제목 -->
+        <v-row justify="center" class="mb-4">
+            <v-col cols="auto">
+                <h1 class="display-1">여행지 정보</h1>
+                <hr>
+            </v-col>
+        </v-row>
+        <MyKakaoMap class="map" />
     </v-container>
-  </template>
-  
-  <script setup>
+</template>
 
+<script setup>
+import MyKakaoMap from '@/components/map/MyKakaoMap.vue';
+</script>
 
-  </script>
-  
-  <style scoped>
-  </style>
-  
+<style scoped>
+.v-container--fluid {
+    max-width: 100%;
+}
+
+</style>
