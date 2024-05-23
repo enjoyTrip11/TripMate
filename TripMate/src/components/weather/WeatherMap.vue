6b1d6839f0d54598b11a520b8bf4cdebee7b1d2c
yeondori <yeonseo.jung0@gmail.com>
2024-05-23 21:26:30 +0900

    fix: 날씨 정보 위치 조정

--- TripMate/src/components/weather/WeatherMap.vue
+++ TripMate/src/components/weather/WeatherMap.vue
@@ -1,20 +1,20 @@
 <template>
-  <v-container fluid>
-    <h2>전국 날씨</h2>
+  <v-container fluid class="container">
+    <h2>실시간 전국 날씨</h2>
     <div class="buttons">
       <div v-for="date in weekDates" :key="date" @click="selectDate(date)" :class="{ 'selected': selectedDate === date }" class="date-button">
         <span v-html="formatDate(date)" :class="{ 'selected': selectedDate === date }"></span>
         <hr :class="{ 'selected': selectedDate === date }">
       </div>
     </div>
-    <v-card class="map-card" max-width="400">
+    <v-card class="map-card">
       <div class="toggle-buttons">
         <v-btn :class="{ 'selected': selectedPeriod === 'AM' }" @click="selectPeriod('AM')">오전</v-btn>
         <v-btn :class="{ 'selected': selectedPeriod === 'PM' }" @click="selectPeriod('PM')">오후</v-btn>
       </div>
       <div class="weather-container">
         <div v-if="loading">
-          <v-progress-circular indeterminate></v-progress-circular>
+          <v-progress-circular class="loadingBar" indeterminate></v-progress-circular>
         </div>
         <div v-else>
             <div v-for="city in cities" :key="city.name" class="weather-info" :style="city.position">
@@ -70,22 +70,22 @@ const formatDate = (date) => {
 };
 
 const cities = ref([
-  { name: '서울', lat: 37.5665, lon: 126.9780, position: { top: '10%', left: '30%' } },
-  { name: '춘천', lat: 37.8813, lon: 127.7298, position: { top: '20%', left: '40%' } },
-  { name: '강릉', lat: 37.7519, lon: 128.8761, position: { top: '30%', left: '50%' } },
-  { name: '청주', lat: 36.6424, lon: 127.4890, position: { top: '40%', left: '60%' } },
-  { name: '울릉/독도', lat: 37.5000, lon: 130.8714, position: { top: '50%', left: '70%' } },
-  { name: '수원', lat: 37.2636, lon: 127.0286, position: { top: '70%', left: '90%' } },
-  { name: '안동', lat: 36.5684, lon: 128.7294, position: { top: '80%', left: '100%' } },
-  { name: '대전', lat: 36.3504, lon: 127.3845, position: { top: '90%', left: '10%' } },
-  { name: '전주', lat: 35.8242, lon: 127.1480, position: { top: '15%', left: '20%' } },
-  { name: '대구', lat: 35.8722, lon: 128.6014, position: { top: '25%', left: '30%' } },
-  { name: '광주', lat: 35.1595, lon: 126.8526, position: { top: '35%', left: '40%' } },
-  { name: '목포', lat: 34.8118, lon: 126.3926, position: { top: '45%', left: '50%' } },
-  { name: '여수', lat: 34.7604, lon: 127.6622, position: { top: '55%', left: '60%' } },
-  { name: '부산', lat: 35.1796, lon: 129.0756, position: { top: '65%', left: '70%' } },
-  { name: '울산', lat: 35.5384, lon: 129.3114, position: { top: '75%', left: '80%' } },
-  { name: '제주', lat: 33.4996, lon: 126.5312, position: { top: '85%', left: '90%' } },
+  { name: '서울', lat: 37.5665, lon: 126.9780, position: { top: '19%', left: '22%' } },
+  { name: '춘천', lat: 37.8813, lon: 127.7298, position: { top: '7%', left: '47%' } },
+  { name: '강릉', lat: 37.7519, lon: 128.8761, position: { top: '16%', left: '62%' } },
+  { name: '청주', lat: 36.6424, lon: 127.4890, position: { top: '20%', left: '48%' } },
+  { name: '울릉/독도', lat: 37.5000, lon: 130.8714, position: { top: '20%', left: '80%' } },
+  { name: '수원', lat: 37.2636, lon: 127.0286, position: { top: '28%', left: '28%' } },
+  { name: '안동', lat: 36.5684, lon: 128.7294, position: { top: '30%', left: '58%' } },
+  { name: '대전', lat: 36.3504, lon: 127.3845, position: { top: '38%', left: '38%' } },
+  { name: '전주', lat: 35.8242, lon: 127.1480, position: { top: '44%', left: '24%' } },
+  { name: '대구', lat: 35.8722, lon: 128.6014, position: { top: '43%', left: '58%' } },
+  { name: '광주', lat: 35.1595, lon: 126.8526, position: { top: '59%', left: '25%' } },
+  { name: '목포', lat: 34.8118, lon: 126.3926, position: { top: '65%', left: '12%' } },
+  { name: '여수', lat: 34.7604, lon: 127.6622, position: { top: '60%', left: '43%' } },
+  { name: '부산', lat: 35.1796, lon: 129.0756, position: { top: '58%', left: '65%' } },
+  { name: '울산', lat: 35.5384, lon: 129.3114, position: { top: '46%', left: '79%' } },
+  { name: '제주', lat: 33.4996, lon: 126.5312, position: { top: '82%', left: '14%' } },
 ]);
 
 const loading = ref(true); // 로딩 상태 추가
@@ -121,11 +121,22 @@ onMounted(async () => {
 </script>
 
 <style scoped>
+.container {
+  margin-left: 20px; /* 왼쪽 마진 설정 */
+  width: 500px;
+}
+
+.container > * {
+  margin-left: 20px; /* 하위 요소의 왼쪽 마진 설정 */
+}
+
 .buttons {
     display: flex;
+    flex-direction: row;
     justify-content: space-around;
-    flex-wrap: wrap; /* 버튼이 한 줄에 다 들어가지 않으면 자동으로 다음 줄에 표시 */
-    margin: 4%;
+    margin-top: 20px; 
+    margin-left: 0px;
+    /* max-width: 450px; */
 }
 
 .date-button {
@@ -135,6 +146,12 @@ onMounted(async () => {
     padding: 0; /* 내부 패딩 제거 */
     cursor: pointer; /* 마우스를 올리면 클릭 가능한 커서로 설정 */
     position: relative; /* 하위 요소에 대한 절대 위치를 설정하기 위해 부모 요소의 상대 위치를 설정 */
+    margin-bottom: 10px;
+}
+
+.loadingBar {
+  position: fixed;
+  top: 60%;
 }
 
 hr {
@@ -161,21 +178,22 @@ hr {
 }
 
 .map-card {
-    margin: 0 auto;
+    margin: 0;
     text-align: center;
     background-color: rgb(208, 243, 255);
     background-image: url('@/assets/img/weather/map.png'); /* 배경 이미지 설정 */
     background-size: cover; /* 이미지 크기를 맞추기 위해 커버로 설정 */
     background-position: center; /* 이미지 위치를 가운데로 설정 */
-    height: 550px; /* 카드 높이 설정 */
+    height: 600px; /* 카드 높이 설정 */ 
     position: relative; /* 버튼을 위치시키기 위해 상대 위치 설정 */
 }
 
+/* 오전/오후 버튼 */
 .toggle-buttons {
     display: flex;
     position: absolute;
-    top: 500px;
-    left: 260px;
+    top: 566px;
+    left: 340px
 }
 
 .toggle-buttons .v-btn {
@@ -198,8 +216,8 @@ hr {
 
 .weather-info {
   position: absolute; /* 절대 위치 설정 */
-  background-color: white; /* 배경색 흰색 설정 */
-  padding: 10px; /* 내부 여백 설정 */
+  /* background-color: white;  배경색 흰색 설정 */
+  padding: 10px; /* 내부 여백 설정 */ 
   padding-bottom: 0;
   border-radius: 10px; /* 둥근 모서리 설정 */
   text-align: center; /* 텍스트 가운데 정렬 */
